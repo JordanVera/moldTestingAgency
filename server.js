d8dd4121c64e4fd6a6abf325d974f78c4b65ad81
Jordan Vera <verawebdevelopment@gmail.com>
2018-07-09 21:06:24 -0500

    Heroku Server PORT Fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
+
 
 app.use(express.static(__dirname + "/public"));
 
