5b5d7d77db99e197f93abe71ab51791b85662a29
Jordan Vera <verawebdevelopment@gmail.com>
2018-07-09 21:22:12 -0500

    Updated Server for Heroku FIX

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const PORT = process.env.PORT || 8080;
 
 
-app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.static(path.join(__dirname + "public")));
 
 app.get("/", (req, res ) => {
 	res.send("index");
